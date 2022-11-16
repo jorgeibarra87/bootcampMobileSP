affdfd97666c56740ab8474f9939eb1b1c8ad1ad
jorgeibarra87 <jorgeibarra87@gmail.com>
2022-11-16 16:05:29 -0500

    second commit

--- src/main/Challenge1.java
+++ src/main/Challenge1.java
@@ -1,9 +1,35 @@
 package main;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Challenge1 {
+	
+	Scanner entry = new Scanner(System.in);
+	static int numbers[]= new int[10];
+	static String stringNumbers="";
+	double promedio=0;
+	
+	public static String stringRandom(String typeString) {
+		if(typeString == "TipoA") {
+			Random myRandom = new Random();
+			for(int i=0;i<10;i++) {
+				numbers[i]=myRandom.nextInt(9);
+			}
+			numbers[0]=5;
+			numbers[1]=4;
+			for(int i=0;i<10;i++) {
+				stringNumbers+=Integer.toString(numbers[i]);
+			}
+		} 
+		return stringNumbers;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+				
+		
+		System.out.println(stringRandom("TipoA"));
 
 	}
 
